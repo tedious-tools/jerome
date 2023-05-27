7a83ff335fb1306a41875145b8417bce974ba06b
Jonathon Anderson <coding@jpanderson.dev>
2023-05-26 23:07:24 -0700

    No longer need quotes, accept all trailing args

---
+++ src/lib.rs
@@ -0,0 +1,97 @@
+use std::collections::HashMap;
+use anyhow::Result;
+
+const ROT13_MAPPING: [(char, char); 26] = [
+    ('a', 'n'),
+    ('b', 'o'),
+    ('c', 'p'),
+    ('d', 'q'),
+    ('e', 'r'),
+    ('f', 's'),
+    ('g', 't'),
+    ('h', 'u'),
+    ('i', 'v'),
+    ('j', 'w'),
+    ('k', 'x'),
+    ('l', 'y'),
+    ('m', 'z'),
+    ('n', 'a'),
+    ('o', 'b'),
+    ('p', 'c'),
+    ('q', 'd'),
+    ('r', 'e'),
+    ('s', 'f'),
+    ('t', 'g'),
+    ('u', 'h'),
+    ('v', 'i'),
+    ('w', 'j'),
+    ('x', 'k'),
+    ('y', 'l'),
+    ('z', 'm'),
+];
+
+pub struct Translator {
+    rot13_mapping: HashMap<char, char>,
+}
+
+impl Translator {
+    pub fn new() -> Translator {
+        Translator{
+            rot13_mapping: HashMap::from(ROT13_MAPPING),
+        }
+    }
+
+    /// Encodes/decodes a given text using a ROT13 cipher. At the moment, this
+    /// loses any case by default. Will likely take na option later of some kind.
+    /// Basically just stores the HashMap so the creation only happens once.
+    ///
+    /// # Examples
+    /// ```
+    /// let original = "Zapper man".to_string();
+    /// assert_eq!("mnccre zna".to_string(), rot13::Translator::new().rot13(&original).unwrap());
+    /// ```
+    pub fn rot13(&self, plaintext: &String) -> Result<String> {
+        let plaintext = plaintext.to_ascii_lowercase();
+
+        let mut translated = String::from("");
+
+        for c in plaintext.chars() {
+            if let Some(found_val) = self.rot13_mapping.get(&c) {
+                translated.push(found_val.clone());
+            } else {
+                translated.push(c);
+            }
+        }
+
+        Ok(translated)
+    }
+}
+
+/// Encodes/decodes a given text using a ROT13 cipher. At the moment, this
+/// loses any case by default. Will likely take na option later of some kind.
+///
+/// # Examples
+/// ```
+/// let original = "Zapper man".to_string();
+/// assert_eq!("mnccre zna".to_string(), rot13::rot13(&original).unwrap());
+/// ```
+pub fn rot13(plaintext: &String) -> Result<String> {
+    Translator::new().rot13(plaintext)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn produces_rot13() {
+        let original = "Sphinx of black quartz, judge my vow.".to_owned();
+        let expected = "fcuvak bs oynpx dhnegm, whqtr zl ibj.".to_owned();
+
+        assert_eq!(expected, rot13(&original).unwrap());
+
+        let translator = Translator::new();
+
+        assert_eq!(expected, translator.rot13(&original).unwrap());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,34 @@
 use std::env;
-use std::collections::HashMap;
 
-const MAPPING: [(char, char); 26] = [
-    ('a', 'n'),
-    ('b', 'o'),
-    ('c', 'p'),
-    ('d', 'q'),
-    ('e', 'r'),
-    ('f', 's'),
-    ('g', 't'),
-    ('h', 'u'),
-    ('i', 'v'),
-    ('j', 'w'),
-    ('k', 'x'),
-    ('l', 'y'),
-    ('m', 'z'),
-    ('n', 'a'),
-    ('o', 'b'),
-    ('p', 'c'),
-    ('q', 'd'),
-    ('r', 'e'),
-    ('s', 'f'),
-    ('t', 'g'),
-    ('u', 'h'),
-    ('v', 'i'),
-    ('w', 'j'),
-    ('x', 'k'),
-    ('y', 'l'),
-    ('z', 'm'),
-];
+use anyhow::{bail, Result};
 
-fn main() -> Result<(), String> {
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    match args.len() {
-        2 => (),
-        1 => return Err("Must provide a string to convert to rot13!".to_string()),
+    if args.len() == 1 {
+        bail!("Must provide a string to convert to rot13!")
+    }
+
+    let msg = match args.len() {
+        0 => bail!("command args hould never be 0, something has gone horribly wrong"),
+        1 => bail!("Must provide a string to convert to rot13!".to_string()),
+        2 => args[1].clone(),
         _ => {
-            return Err(
-                "Found more than one argument. Please wrap your string in \"'s if it has spaces"
-                    .to_string(),
-            )
-        }
+            let text = &args[1..];
+
+            text[2..].iter().fold(args[1].clone(), |accumulator, val| format!("{} {}", accumulator, val))
+        },
     };
 
-    let mapped = HashMap::from(MAPPING);
-    let msg = args[1].to_ascii_lowercase();
-    let mut translated = String::from("");
-
-    for c in msg.chars() {
-        if let Some(found_val) = mapped.get(&c) {
-            translated.push(found_val.clone());
-        } else {
-            translated.push(c);
-        }
-    }
+    let translated = rot13::rot13(&msg)?;
 
     println!("Translated gets you: \n{}", translated);
 
-    Ok(())
+    match cli_clipboard::set_contents(translated) {
+        Err(_) => bail!("\n==> Unable to copy to clipboard".to_string()),
+        Ok(_) => {
+            println!("Successfully copied to clipboard");
+            return Ok(())
+        },
+    };
 }
